3e6bcfa3557b50d6af0f913e9e899e004954008d
Jemy Chen <jemychen@live.com>
2020-01-22 21:41:43 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -21,5 +21,5 @@ def print_first_directors_movie_titles
     column_index += 1
   end
   #pp(nds)
-  #p (nds[0][column_index][inner_index][inner_index])
+  p (nds[0][column_index][inner_index][inner_index])
 end
